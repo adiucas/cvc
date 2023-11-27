6e2bd8bc5cd544a99dd878ee663c46a5e94d3520
adiucas <adi@ucabd.com>
2023-11-27 15:30:23 +0600

    werfwefger

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('.first-option').hide();
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     responsiveClass:true,
+//     autoplay: true,
+//     autoplayTimeout: 3000,
+//     autoplayHoverPause:true,
+//     dots: false,
+//     margin: 15,
+//     center: true,
+//     autoWidth:true
+// })
